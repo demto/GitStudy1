ec200a7a9e2bf63e5a0e0332055e1e5e0622bb63
Tom <demto13@gmail.com>
2018-02-03 21:06:51 +0000

    Added comment to test if it appears in branch only

--- GitStudy1/Program.cs
+++ GitStudy1/Program.cs
@@ -10,6 +10,7 @@ namespace GitStudy1 {
 
             Console.WriteLine("Hello World!");
             Console.ReadLine();
+            // This comment should only exist in the support_20180203 branch
         }
     }
 }
